6b375b793fedb9c6d19ecf8503bc5cb0146fc0d4
gaebor <gaebor@gmail.com>
2019-04-01 18:02:56 +0200

    delta rename and bugfix

--- segmented.cpp
+++ segmented.cpp
@@ -22,15 +22,11 @@ constexpr Ty Round(Ty x)
 
 template<class bitset>
 void calculate_sieve(size_t n,
-    size_t delta = 0, size_t delta2 = 0, size_t thread = 1,
+    size_t delta0 = 0, size_t delta = 0, size_t thread = 1,
     const std::string& savefilename = "")
 {
-    delta = std::max(size_t(std::ceil(std::sqrt(n))), delta);
-    delta = std::min(delta, n);
-    delta = Round<bitset::word_size>(delta);
-
-    if (delta2 == 0)
-        delta2 = delta;
+    delta0 = Round<bitset::word_size>(std::min<size_t>(std::max<size_t>(std::ceil(std::sqrt(n)), delta0), n));
+    delta = Round<bitset::word_size>(std::min<size_t>(std::max<size_t>(std::ceil(std::sqrt(n)), delta), n));
 
     std::vector<size_t> found_primes;
 
@@ -63,39 +59,33 @@ void calculate_sieve(size_t n,
     }
 
     std::list<bitset> tables;
-    tables.emplace_back(delta);
-    found_primes.reserve(size_t(delta / std::log(delta)));
+    tables.emplace_back(delta0);
+    found_primes.reserve(size_t(delta0 / std::log(delta0)));
 
-    CalculateSieve(delta, tables.front() , [&found_primes](size_t x) {found_primes.push_back(x); });
+    CalculateSieve(delta0, tables.front() , [&found_primes](size_t x) {found_primes.push_back(x); });
     printer(tables.front(), 1);
 
     for (size_t i = 1; i < thread; ++i)
-        tables.emplace_back(delta2);
+        tables.emplace_back(delta);
     
     std::vector<std::thread> threads(thread);
 
-    //for (m = delta + 1; m < n; m += delta2)
-    //{
-    //    CalculateSegment(m, std::min(m + delta2 - 1, n), found_primes, tables[0]);
-    //    printer(tables[0]);
-    //}
-
-    size_t m = delta + 1;
+    size_t m = delta0 + 1;
     while (m < n)
     {
         auto table_it = tables.begin();
         for (size_t t = 0; t < thread; ++t, ++table_it)
         {
-            if (m < n)
+            if (m + t * delta < n)
                 threads[t] = std::thread([&found_primes](size_t a, size_t b, bitset* table)
             {
                 CalculateSegment<bitset>(a, b, found_primes, *table);
-            }, m + t * delta2, std::min(m + (t + 1) * delta2 - 1, n), &*table_it);
+            }, m + t * delta, std::min(m + (t + 1) * delta - 1, n), &*table_it);
             else
                 break;
         }
         table_it = tables.begin();
-        for (size_t t = 0; t < thread; ++t, m += delta2)
+        for (size_t t = 0; t < thread; ++t, m += delta)
         {
             if (threads[t].joinable())
             {
@@ -115,8 +105,8 @@ int main(int argc, const char* argv[])
     size_t wordsize = CHAR_BIT * sizeof(void*);
     size_t n = 1 << 10;
     auto main_function = calculate_sieve<Bitset<std::uint32_t>>;
+    size_t delta0 = 0;
     size_t delta = 0;
-    size_t delta2 = 0;
     size_t thread = 1;
 
     arg::Parser parser("Prime counting application using segmented sieve of Jonathan Sorenson\n"
@@ -126,7 +116,7 @@ int main(int argc, const char* argv[])
     parser.AddArg(delta, { "-d", "--delta" }, 
         "length of one segment in the segmented sieve"
         "\n\t\tif not set, then sqrt(n) is used."
-        "\n\t\tIt is advised to set it to 4 times the L2 cache size in bytes."
+        "\n\t\tIt is advised to set it to 4 times the (per-core) L2 cache size in bytes."
         "\n\t\tFor example: 1MiByte -> delta=2^22");
     parser.AddArg(outfilename, { "-o", "--output" },
         "binary output filename, if empty then print text to stdout", "string");
@@ -136,8 +126,8 @@ int main(int argc, const char* argv[])
         "resets blocked flag to vanilla computing", false);
     parser.AddArg<size_t>(wordsize, { "-w", "--word", "-s", "--storage" },
         "internal representation size in bits", "", {8,16,32,64});
-    parser.AddArg(delta2, { "-d2", "--delta2" });
-    parser.AddArg(thread, { "-t", "--thread", "--threads" });
+    parser.AddArg(delta0, { "-d0", "--delta0" });
+    parser.AddArg(thread, { "-t", "--thread", "--threads" }, "The memory usga is ");
     
     parser.Do(argc, argv);
 
@@ -162,7 +152,7 @@ int main(int argc, const char* argv[])
         };
     }
 
-    main_function(n, delta, delta2, thread, outfilename);
+    main_function(n, delta0, delta, thread, outfilename);
 
     return 0;
 }
