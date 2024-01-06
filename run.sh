386acc45bc419c1a899cc1a9bb2ad5a162d60605
Gaebor <gaebor@gmail.com>
2024-01-06 23:16:45 +0100

    optional run env installation

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ top_level_dir=`pwd`
 ls -d -1 */ | while read directory
 do
     cd "$top_level_dir/$directory"
-    bash run_env.sh
+    if [ -f run_env.sh ]; then bash run_env.sh; fi
     python3 "$top_level_dir/run.py"
 done
