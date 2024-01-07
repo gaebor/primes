2776fa8ef8c05931de4f4557f052870651666447
Gaebor <gaebor@gmail.com>
2024-01-07 11:43:57 +0100

    fix copy

--- cpp/build.ps1
+++ cpp/build.ps1
@@ -1,4 +1,4 @@
 cmake .
 msbuild primes.sln /p:Configuration=Release
 
-Copy-Item -Filter *.exe -Path ".\Release" -Destination ".\"
+Copy-Item -Filter "*.exe" -Path ".\Release\*" -Recurse -Destination ".\"
